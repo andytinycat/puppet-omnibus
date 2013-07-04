4c202a0ada5061d0398852f5275f2d39e88f1156
Jan Ivar Beddari <jan.ivar.beddari@uib.no>
2013-07-04 23:36:48 +0200

    Initscripts does not work yet as I'm not sure how to best split them out per platform yet

--- initscripts.rb
+++ initscripts.rb
@@ -1,22 +1,23 @@
-class InitScript < FPM::Cookery::Recipe
-  description 'Install Puppet init script'
+class InitScripts < FPM::Cookery::Recipe
+  description 'Installs Puppet init scripts'
 
-  name 'init-script'
+  name 'initscripts'
   version '1.0.0'
   source "nothing", :with => :noop
+
+  platforms [:ubuntu, :debian] do
+    #etc('init.d').install workdir("puppet.init.d") => 'puppet'
+  end
+  platforms [:fedora, :redhat, :centos] do
+    #etc('init.d').install workdir("files/redhat/etc/init.d/puppet") => 'puppet'
+    #etc('sysconfig').install workdir("files/redhat/etc/sysconfig/puppet") => 'puppet'
+  end
 
   def build
     # Do nothing
   end
 
   def install
-    # Copy init-scripts to right place
-#    platforms [:ubuntu, :debian] do
-#      etc('init.d').install workdir("puppet.init.d") => 'puppet'
-#    end
-#    platforms [:fedora, :redhat, :centos] do
-      etc('init.d').install workdir("../files/redhat/etc/init.d/puppet") => 'puppet'
-      etc('sysconfig').install workdir("../files/redhat/etc/sysconfig/puppet") => 'puppet'
-#    end
+    # Do nothing 
   end
 end
