157cc2eb00847acddc16fc49951986eb44588c63
Jan Ivar Beddari <jan.ivar.beddari@uib.no>
2013-07-06 15:41:36 +0200

    Bring aws recipe in line with current changes

--- recipe-aws.rb
+++ recipe-aws.rb
@@ -5,7 +5,7 @@ class PuppetOmnibus < FPM::Cookery::Recipe
   name 'puppet-omnibus'
   version '3.2.2'
   description 'Puppet Omnibus package'
-  revision 2
+  revision 0
   vendor 'fpm'
   maintainer '<github@tinycat.co.uk>'
   license 'Apache 2.0 License'
@@ -13,79 +13,32 @@ class PuppetOmnibus < FPM::Cookery::Recipe
   source '', :with => :noop
 
   omnibus_package true
+  omnibus_dir     "/opt/#{name}"
   omnibus_recipes 'libyaml',
                   'ruby',
                   'puppet',
-                  'initscripts',
                   'aws'
 
-  omnibus_dir              '/opt/puppet-omnibus'
-  omnibus_additional_paths '/etc/init.d/puppet'
-  platforms [:fedora, :redhat, :centos] do
-    omnibus_additional_paths.push('/etc/sysconfig/puppet')
+  # Set up paths to initscript and config files per platform
+  platforms [:ubuntu, :debian] do
+    config_files '/etc/puppet/puppet.conf',
+                 '/etc/init.d/puppet',
+                 '/etc/default/puppet'
   end
+  platforms [:fedora, :redhat, :centos] do
+    config_files '/etc/puppet/puppet.conf',
+                 '/etc/init.d/puppet',
+                 '/etc/sysconfig/puppet'
+  end
+  omnibus_additional_paths config_files
 
   def build
     # Nothing
   end
 
   def install
-    rm_rf "#{destdir}/bin"
-    # Provide 'safe' binaries like Vagrant does
-    destdir('bin').mkdir
-    destdir('bin').install workdir('files/omnibus.bin'), 'puppet'
-    destdir('bin').install workdir('files/omnibus.bin'), 'facter'
-    destdir('bin').install workdir('files/omnibus.bin'), 'hiera'
-
-    # Symlink binaries to PATH using update-alternatives
-    with_trueprefix do
-      create_post_install_hook
-      create_pre_uninstall_hook
-    end
+    # Nothing
   end
 
-  private
-
-  def create_post_install_hook
-    File.open(builddir('post-install'), 'w', 0755) do |f|
-      f.write <<-__POSTINST
-#!/bin/sh
-set -e
-
-BIN_PATH="#{destdir}/bin"
-BINS="puppet facter hiera"
-
-for BIN in $BINS; do
-  update-alternatives --install /usr/bin/$BIN $BIN $BIN_PATH/$BIN 100
-done
-
-exit 0
-      __POSTINST
-
-      self.class.post_install(File.expand_path(f.path))
-    end
-  end
-
-  def create_pre_uninstall_hook
-    File.open(builddir('pre-uninstall'), 'w', 0755) do |f|
-      f.write <<-__PRERM
-#!/bin/sh
-set -e
-
-BIN_PATH="#{destdir}/bin"
-BINS="puppet facter hiera"
-
-if [ "$1" != "upgrade" ]; then
-  for BIN in $BINS; do
-    update-alternatives --remove $BIN $BIN_PATH/$BIN
-  done
-fi
-
-exit 0
-      __PRERM
-
-      self.class.pre_uninstall(File.expand_path(f.path))
-    end
-  end
 end
 
