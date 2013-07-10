3b3fc52da93944f12c0ff4666405739cbc1abfbd
Jan Ivar Beddari <jan.ivar.beddari@uib.no>
2013-07-10 10:04:17 +0200

    Add gem to support deep merge in Hiera 1.2.0+, see http://docs.puppetlabs.com/hiera/1/lookup_types.html

--- puppet.rb
+++ puppet.rb
@@ -21,6 +21,7 @@ class PuppetGem < FPM::Cookery::Recipe
     gem_install 'facter',      '1.7.1'
     gem_install 'json_pure',   '1.6.3'
     gem_install 'hiera',       '1.2.1'
+    gem_install 'deep_merge',  '1.0.0'
     gem_install 'rgen',        '0.6.5'
     gem_install 'ruby-augeas', '0.4.1'
     gem_install 'ruby-shadow', '2.2.0'
