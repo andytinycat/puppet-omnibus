527985877cd53b9d72d3ffb19e00bf99b8ce0c02
Jon Topper <jon@scalefactory.com>
2014-04-23 13:42:09 +0100

    Correct the provides config

--- recipe.rb
+++ recipe.rb
@@ -21,7 +21,7 @@ class PuppetOmnibus < FPM::Cookery::Recipe
 
   conflicts automation_packages
   replaces  automation_packages
-  provides  [ automation_packages, 'automation_ruby(abi)' ].flatten
+  provides  [ automation_packages, 'automation-ruby(abi) = 2.0' ].flatten
 
   source '', :with => :noop
 
