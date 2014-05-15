e309679fcc024efbd20908bbc22b4b9fcf0d99e7
Ryan Blair <rblair@gmail.com>
2014-05-14 21:19:54 -0700

    update libyaml to current

--- libyaml.rb
+++ libyaml.rb
@@ -2,11 +2,11 @@ class Ruby193 < FPM::Cookery::Recipe
   description 'LibYAML is a YAML 1.1 parser and emitter written in C'
 
   name 'libyaml'
-  version '0.1.4'
+  version '0.1.5'
   revision 1
   homepage 'http://pyyaml.org/wiki/LibYAML'
-  source 'http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz'
-  sha256 '7bf81554ae5ab2d9b6977da398ea789722e0db75b86bffdaeb4e66d961de6a37'
+  source 'http://pyyaml.org/download/libyaml/yaml-0.1.5.tar.gz'
+  sha256 'fa87ee8fb7b936ec04457bc044cd561155e1000a4d25029867752e543c2d3bef'
 
   maintainer '<beddari@deploy.no>'
   vendor     'fpm'
